42075bba981f3d4ade9b9659aa33a281906826f7
hamid2117 <saim008865@gmail.com>
2021-08-28 15:30:16 +0500

    first cfsdafsadf

--- src/auth/genrateToken.js
+++ src/auth/genrateToken.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 const generateToken = (id) => {
   // return jwt.sign({ id }, process.env.JWT_SECRET, {
   return jwt.sign({ id }, 'dimahdani9530', {
-    expiresIn: process.env.JWT_EXPIRE_TIME,
+    expiresIn: '30d',
   })
 }
 
