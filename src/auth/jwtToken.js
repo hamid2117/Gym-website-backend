432c6b3633b94c125da2cc96e0d11932cfb34d23
hamid2117 <saim008865@gmail.com>
2021-08-29 07:44:34 +0500

    first cfsdafsadf

--- src/auth/jwtToken.js
+++ src/auth/jwtToken.js
@@ -5,9 +5,7 @@ const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken()
 
   const option = {
-    expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
-    ),
+    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     httpOnly: true,
   }
   res.status(statusCode).cookie('token', token, option).json({
