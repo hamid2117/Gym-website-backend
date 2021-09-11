e0fd232785c32c5634ac953a8c09afeaa6e92156
hamid2117 <saim008865@gmail.com>
2021-09-11 13:15:09 +0500

    added a api

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import User from '../models/userModel.js'
+import CourseModel from '../models/courseModel.js'
 import asyncHandler from 'express-async-handler'
 import { protect, admin } from './../auth/authMiddleware.js'
 const router = express.Router()
@@ -30,6 +31,29 @@ router.get(
     const trianer = user.filter((data) => {
       return data.trainer === true
     })
+    if (trianer) {
+      res.json(trianer)
+    } else {
+      res.status(404)
+      throw new Error('User not Found')
+    }
+  })
+)
+router.get(
+  '/trainercourses/:id',
+  protect,
+  asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id).select([
+      '-password',
+      '-isAdmin',
+      '-createdAt',
+      '-updatedAt',
+    ])
+    const courses = await CourseModel.find({ user: req.params.id })
+    const trianer = {
+      user,
+      courses,
+    }
     if (trianer) {
       res.json(trianer)
     } else {
